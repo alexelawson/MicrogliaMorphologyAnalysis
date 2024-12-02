5ba8c09f47c7826388ac54b507653b4c4a41a1a5
Alexandra Lawson <alexelaws@outlook.com>
2024-12-01 19:10:07 -0800

    Updated commenting and cleaning code

--- homeostatic-analysis.R
+++ homeostatic-analysis.R
@@ -39,15 +39,15 @@ sampling <- kmeans_input[sample(nrow(kmeans_input), 5000),] #sample 5000 random 
 fviz_nbclust(sampling, kmeans, method = 'silhouette', nstart=25, iter.max=50) # 4 clusters
 data_kmeans <- kmeans(kmeans_input, centers=4)
 
-# Here, we are creating a new data frame that contains the first 2 PCs and original dataset, then renaming the data_kmeans$cluster column to simply say "Cluster". You can bind together as many of the PCs as you want. Binding the original, untransformed data is useful if you want to plot the raw values of any individual morphology measures downstream. 
+#Creating a new data frame that contains the first 2 PCs and original dataset, then renaming the data_kmeans$cluster column to simply say "Cluster"
 pca_kmeans <- cbind(pca_data[1:2], combined_data, as.data.frame(data_kmeans$cluster)) %>%
   rename(Cluster=`data_kmeans$cluster`)
 
 #display cluster features in a heat map + plot 
 clusterfeatures(pca_kmeans_w_cluster, featurestart=9, featureend=35)
-
 plot <- clusterplots(pca_kmeans, "PC1", "PC2")
 
+#view PCA feature correlations and how they explain each morphometric component
 pcfeaturecorrelations(pca_data, pc.start=1, pc.end=3, 
                       feature.start=17, feature.end=43, 
                       rthresh=0.75, pthresh=0.05, 
@@ -72,39 +72,35 @@ pca_kmeans_w_cluster <- pca_kmeans %>% mutate(Cluster =
                                                           Cluster=="1" ~ "Rod-Like"))
                                                 
 
-#Ensure 'Sex' and 'Treatment' are factors
+#Ensuring relevant columns are factors for statistical analysis 
 stats_input <- cp
 stats_input$MouseID <- factor(stats_input$MouseID)
 stats_input$Cluster <- factor(stats_input$Cluster)
 stats_input$Treatment <- factor(stats_input$Treatment)
+
+#linear mixed model with 2 post-hoc outputs 
 stats_output <- stats_cluster.animal(data = stats_input, 
                                        model = "percentage ~ Sex*Treatment*Cluster + (1|MouseID)", 
                                        posthoc1 = "~Treatment|Cluster|Sex", 
                                        posthoc2 = "~Sex|Cluster|Treatment")
 
+
+#saving stats outputs 
 treatment_cluster_stats <- stats_output[[2]]
 write.csv(treatment_cluster_stats, "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/statistical-results/hard-cluster-post-hoc1.csv", row.names = FALSE)
 sex_cluster_stats <- stats_output[[3]]
 write.csv(sex_cluster_stats, "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/statistical-results/hard-cluster-post-hoc2.csv", row.names = FALSE)
 
-# Load the datasets
-cp_noID <- clusterpercentage(pca_kmeans, "Cluster", Sex, Treatment)
-cp_noID <- cp_noID %>% mutate(Cluster = 
-                      case_when(Cluster=="4" ~ "Ramified",
-                                Cluster=="3" ~ "Ameboid",
-                                Cluster=="2" ~ "Hypertrophic",
-                                Cluster=="1" ~ "Rod-Like"))
 
-data_graph <- cp_noID         # Original data
-significance_data <- stats_output[[2]]   # Statistical significance information for treatment vs. sex vs. cluster
+significance_data <- stats_output[[2]]   # Statistical significance information for treatment vs. cluster vs. sex
 
-# Create a new column to flag significant bars
+#New column to flag significant bars
 significance_data <- significance_data %>%
   mutate(
-    outline_color = ifelse(Significant == "significant", "red", "black")  # Red outline for significant bars
+    outline_color = ifelse(Significant == "significant", "red", "black")  # Red outline for significant bars, black for others 
   )
 
-# Create the plot
+#Creating the plot
 plot_hard_cluster <- ggplot(significance_data, aes(x = interaction(Cluster, contrast), y = estimate, fill = Sex)) +
   geom_bar(
     stat = "identity", 
@@ -125,32 +121,32 @@ plot_hard_cluster <- ggplot(significance_data, aes(x = interaction(Cluster, cont
   theme_minimal() +
   theme(
     text = element_text(size = 14),
-    axis.text.x = element_text(angle = 45, hjust = 1)  # Rotate x-axis labels for readability
+    axis.text.x = element_text(angle = 45, hjust = 1)  #Rotate x-axis labels for readability
   ) +
   scale_fill_manual(
-    values = c("F" = "white", "M" = "lightgrey")  # Custom colors for Female and Male
+    values = c("F" = "white", "M" = "lightgrey")  #Custom colors for Female and Male
   ) +
-  scale_color_identity()  # Use color directly from the outline_color column
+  scale_color_identity()  #Use color directly from the outline_color column
 
 # Display the plot
 print(plot_hard_cluster)
+#Saving the plot 
 ggsave(filename = "hard-cluster-plot.png", plot = plot_hard_cluster, path = "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/plots-and-images", width = 8, height = 6, dpi = 300)
 
+#Fuzzy clustering code lines commented out because the process is very time intensive
+#The output was saved in an RDS file for later use 
 #fuzzy cluster analysis
 #data_kmeans <- fcm(kmeans_input, centers=4, nstart=25)
 #View(data_kmeans)
-
-#saving anlysis as RDS as it takes a long time to run a fuzzy cluster analysis 
 #saveRDS(data_kmeans, "fuzzy_clustering.rds")
 
-# Load the object
+#Load the fuzzy cluster object from the saved location 
 fuzzy_clustering <- readRDS("/Users/alexlawson/Downloads/fuzzy_clustering.rds")
-
+#Extracting the columns with membership percentages
 memberships <- fuzzy_clustering$u
 membership_df <- as.data.frame(memberships)
-View(membership_df)
 
-# Here, we are creating a new data frame that contains the first 2 PCs and original dataset, then renaming the data_kmeans$cluster column to simply say "Cluster". You can bind together as many of the PCs as you want. Binding the original, untransformed data is useful if you want to plot the raw values of any individual morphology measures downstream. 
+#Creating a new data frame that contains the first 2 PCs and original dataset + renaming the data_kmeans$cluster column to simply say "Cluster"
 fuzzy_cluster_data <- cbind(pca_data[1:2], combined_data, membership_df, pca_kmeans_w_cluster$Cluster) %>%
   rename(Cluster=`pca_kmeans_w_cluster$Cluster`)
 
@@ -159,12 +155,15 @@ fuzzy_cluster_data_filtered <- fuzzy_cluster_data %>%
            `Cluster 2` > 0.70|
            `Cluster 3` > 0.70|
            `Cluster 4` > 0.70)
+#Viewing the cluster features for analysis 
 clusterfeatures(fuzzy_cluster_data_filtered, featurestart=9, featureend=35)
 
+#Calculating the cluster percentages
 cp_fuzzy <- clusterpercentage(fuzzy_cluster_data_filtered, "Cluster", MouseID, Treatment, Sex)
-cp_fuzzy_noID <- clusterpercentage(fuzzy_cluster_data_filtered, "Cluster", Treatment, Sex)
 View(cp_fuzzy)
 
+
+#Linear mixed model for fuzzy data
 stats_input_fuzzy <- cp_fuzzy
 stats_input_fuzzy$MouseID <- factor(stats_input_fuzzy$MouseID)
 stats_input_fuzzy$Cluster <- factor(stats_input_fuzzy$Cluster)
@@ -174,6 +173,7 @@ stats_output_fuzzy <- stats_cluster.animal(data = stats_input_fuzzy,
                                      posthoc1 = "~Treatment|Cluster|Sex", 
                                      posthoc2 = "~Sex|Cluster|Treatment")
 
+#Saving the output from the fuzzy cluster analysis 
 stats_treatment_cluster_fuzzy <- stats_output_fuzzy[[2]]
 write.csv(stats_treatment_cluster_fuzzy, "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/statistical-results/fuzzy-cluster-post-hoc1.csv", row.names = FALSE)
 stats_sex_fuzzy <- stats_output_fuzzy[[3]]
@@ -181,13 +181,13 @@ write.csv(stats_sex_fuzzy, "/Users/alexlawson/Documents/GitHub/MicrogliaMorpholo
 
 
 
-# Create a new column to flag significant bars
+#Ncolumn to flag significant bars
 significance_data_fuzzy <- stats_output_fuzzy[[2]] %>%
   mutate(
     outline_color = ifelse(Significant == "significant", "red", "black")  # Red outline for significant bars
   )
 
-# Create the plot
+#Creating the plot
 plot_fuzzy <- ggplot(significance_data_fuzzy, aes(x = interaction(Cluster, contrast), y = estimate, fill = Sex)) +
   geom_bar(
     stat = "identity", 
@@ -215,13 +215,14 @@ plot_fuzzy <- ggplot(significance_data_fuzzy, aes(x = interaction(Cluster, contr
   ) +
   scale_color_identity()  # Use color directly from the outline_color column
 
-# Display the plot
+#Display the plot
 print(plot_fuzzy)
+
+#Saving the plot 
 ggsave(filename = "fuzzy-cluster-plot.png", plot = plot_fuzzy, path = "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/plots-and-images", width = 8, height = 6, dpi = 300)
-clusterfeatures(pca_kmeans, featurestart=9, featureend=35)
 cluster_column <- pca_kmeans_w_cluster$Cluster
 
-# Create a new column to flag significant bars
+#Creating a plot to visualize the ramified cluster data
 significance_data_fuzzy <- stats_output_fuzzy[[2]] %>%
   mutate(
     outline_color = ifelse(Significant == "significant", "red", "black")  # Red outline for significant bars
@@ -255,18 +256,19 @@ plot_fuzzy_ramified <- ggplot(significance_data_fuzzy %>% filter(Cluster == "Ram
   ) +
   scale_color_identity()  # Use color directly from the outline_color column
 
-# Display the plot
+#Displaying the plot
 print(plot_fuzzy_ramified)
 
+#Extracting only the ramified column from the original hard cluster set 
 ramified_data <- cbind(normalized_combined_data, cluster_column) %>% filter(cluster_column=="Ramified")
-#calculating pca data
+#calculating pca data for the ramified data 
 pca_data_ramified <- pcadata(ramified_data, featurestart=7, featureend=33,
                     pc.start=1, pc.end=10)
 
-#normalizing first 3 PCA's
+#normalizing first 4 PCA's
 pca_data_scale_ramified <- transform_scale(pca_data_ramified, start=1, end=4) # scale pca data as input for k-means clustering
 
-#kmeans sampling + kmeans clustering
+#kmeans sampling + kmeans clustering for the ramified cluster with silhouette + wss to determine optimal number of clusters 
 kmeans_input_ramified <- pca_data_scale_ramified[1:4]
 sampling_ramified <- kmeans_input_ramified[sample(nrow(kmeans_input_ramified), 5000),] #sample 5000 random rows for cluster optimization
 fviz_nbclust(sampling_ramified, kmeans, method = 'silhouette', nstart=25, iter.max=50) # 5 clusters
@@ -274,7 +276,7 @@ fviz_nbclust(sampling_ramified, kmeans, method = 'wss', nstart=25, iter.max=50)
 data_kmeans_ramified <- kmeans(kmeans_input_ramified, centers=5)
 
 
-# Here, we are creating a new data frame that contains the first 2 PCs and original dataset, then renaming the data_kmeans$cluster column to simply say "Cluster". You can bind together as many of the PCs as you want. Binding the original, untransformed data is useful if you want to plot the raw values of any individual morphology measures downstream. 
+#Creating a new data frame that contains the first 2 PCs and original dataset, then renaming the data_kmeans$cluster column to simply say "Cluster"
 pca_kmeans_ramified <- cbind(pca_data_ramified[1:2], ramified_data, as.data.frame(data_kmeans_ramified$cluster)) %>%
   rename(Cluster=`data_kmeans_ramified$cluster`)
 
@@ -282,16 +284,19 @@ pca_kmeans_ramified <- cbind(pca_data_ramified[1:2], ramified_data, as.data.fram
 clusterfeatures(pca_kmeans_ramified, featurestart=9, featureend=35)
 plot <- clusterplots(pca_kmeans, "PC1", "PC2")
 
+
+#Linear mixed model stats for the ramified cluster 
 stats_input_ramified <- clusterpercentage(pca_kmeans_ramified, "Cluster", Sex, Treatment, MouseID)
 stats_input_ramified$MouseID <- factor(stats_input_ramified$MouseID)
 stats_input_ramified$Cluster <- factor(stats_input_ramified$Cluster)
 stats_input_ramified$Treatment <- factor(stats_input_ramified$Treatment)
-
 stats_output_ramified <- stats_cluster.animal(data = stats_input_ramified, 
                                            model = "percentage ~ Treatment*Cluster*Sex + (1|MouseID)", 
                                            posthoc1 = "~Treatment|Cluster|Sex", 
                                            posthoc2 = "~Treatment|Cluster")
 
+
+#Saving the ramified cluster stats 
 anova_stats_ramified <- stats_output_ramified[[1]]
 write.csv(anova_stats_ramified, "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/statistical-results/anova-stats-ramified-cluster.csv", row.names = FALSE)
 treatment_cluster_stats_ramified <- stats_output_ramified[[2]]
@@ -299,13 +304,13 @@ write.csv(treatment_cluster_stats_ramified, "/Users/alexlawson/Documents/GitHub/
 sex_cluster_stats_ramified <- stats_output_ramified[[3]]
 write.csv(sex_cluster_stats_ramified, "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/statistical-results/hard-cluster-ramified-post-hoc2.csv", row.names = FALSE)
 
-# Create a new column to flag significant bars
+#New column to flag significant bars
 significance_data_ramified <- stats_output_ramified[[2]] %>%
   mutate(
     outline_color = ifelse(Significant == "significant", "red", "black")  # Red outline for significant bars
   )
 
-# Create the plot
+#Creating the plot 
 plot_ramified_all <- ggplot(significance_data_ramified, aes(x = interaction(Cluster, contrast), y = estimate, fill = Sex)) +
   geom_bar(
     stat = "identity", 
@@ -333,13 +338,14 @@ plot_ramified_all <- ggplot(significance_data_ramified, aes(x = interaction(Clus
   ) +
   scale_color_identity()  # Use color directly from the outline_color column
 
-# Display the plot
+#Displaying the plot
 print(plot_ramified_all)
-
+#Saving the plot 
 ggsave(filename = "ramified_all.png", plot = plot_ramified_all, path = "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/plots-and-images", width = 8, height = 6, dpi = 300)
 
 
-# Create a new column to flag significant bars and reorder levels
+#New plot to just visualize the clusters of interest 
+#New column to flag significant bars and reorder levels
 significance_data_ramified <- stats_output_ramified[[3]] %>%
   mutate(
     outline_color = ifelse(Significant == "significant", "red", "black"),  # Red outline for significant bars
@@ -351,7 +357,7 @@ significance_data_ramified <- stats_output_ramified[[3]] %>%
     Cluster_Contrast = factor(Cluster_Contrast, levels = unique(Cluster_Contrast))  # Set factor levels
   )
 
-# Create the plot
+#Creating the plot 
 plot_ramified <- ggplot(significance_data_ramified %>% filter((Cluster %in% c(1, 2))), aes(x = Cluster_Contrast, y = estimate)) +
   geom_bar(
     stat = "identity", 
@@ -376,14 +382,15 @@ plot_ramified <- ggplot(significance_data_ramified %>% filter((Cluster %in% c(1,
   scale_color_identity() +  # Use color directly from the outline_color column
   scale_fill_identity()     # Use fill directly from the fill_color column
 
+#Printing the plot
 print(plot_ramified)
-
+#Saving the plot 
 ggsave(filename = "ramified_1_and_2_plot.png", plot = plot_ramified, path = "/Users/alexlawson/Documents/GitHub/MicrogliaMorphologyAnalysis/plots-and-images", width = 8, height = 6, dpi = 300)
 
 
 
 
-
+#Supervised cluster analysis purely for investifating purposes 
 ########################
 #Supervised-analysis
 normalized_supervised_data <-transform_log(combined_data, 1, start=7, end=33) 
